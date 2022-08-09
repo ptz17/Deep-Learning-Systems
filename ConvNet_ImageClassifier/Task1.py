b7cfd7bcf322fe46f664d9e5c75ae7e0e71db1f3
Princess Tara Zamani <42658032+ptz17@users.noreply.github.com>
2022-08-09 15:27:19 -0600

    Delete Task1.py

--- ConvNet_ImageClassifier/Task1.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow import keras
-
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, BatchNormalization
-from tensorflow.keras import Model
-import numpy as np
-import matplotlib.pyplot as plt
-
